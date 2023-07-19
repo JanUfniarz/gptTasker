d5db8bfc934602cb36c66f2286bfa7c1dd52ce53
Jan Ufniarz <jan.ufniarz@gmail.com>
2023-07-19 23:52:30 +0200

    Tutorial class cd

--- backend/src/main/java/com/example/backend/gpt/tutorial/Tutorial.java
+++ backend/src/main/java/com/example/backend/gpt/tutorial/Tutorial.java
@@ -1,11 +1,17 @@
 package com.example.backend.gpt.tutorial;
 
+import com.example.backend.gpt.PrimaryColor;
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import java.util.List;
+import java.util.Objects;
 
 @Entity
 @Table
+@NoArgsConstructor
 public class Tutorial {
 
     @Id
@@ -20,8 +26,14 @@ public class Tutorial {
     )
     private Long id;
 
+    @Getter
+    @Setter
+    private PrimaryColor primaryColor;
+
+    @Getter
     private String topic;
 
+    @SuppressWarnings("FieldMayBeFinal")
     @ElementCollection
     @CollectionTable(
             name = "paragraphs",
@@ -31,4 +43,36 @@ public class Tutorial {
     )
     @Embedded
     private List<Paragraph> paragraphs;
+
+    public Tutorial(String topic, List<Paragraph> paragraphs) {
+        this.topic = topic;
+        this.paragraphs = paragraphs;
+        this.primaryColor = PrimaryColor.GRAY;
+    }
+
+    public List<Paragraph> getParagraphs() {
+        return List.copyOf(paragraphs);
+    }
+
+    public void replaceParagraphBody(Paragraph newParagraph) {
+        paragraphs.forEach(paragraph -> {
+            if (Objects.equals(
+                    paragraph.getHeadline(), newParagraph.getHeadline()
+            )) paragraph.setBody(
+                    newParagraph.getBody()
+            );
+        });
+    }
+
+    public void removeParagraph(Paragraph paragraph) {
+        int index = -1;
+        for (int i = 0; i < paragraphs.size(); i++) {
+            if (paragraphs.get(i).equals(paragraph)) {
+                index = i;
+                break;
+            }
+        }
+
+        if (index != -1) paragraphs.remove(index);
+    }
 }
